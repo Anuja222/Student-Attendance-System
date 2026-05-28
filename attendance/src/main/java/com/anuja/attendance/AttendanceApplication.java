8eda9266715c1bc3040ed47bb49f7ceb68701122
Anuja Wanigasekara <anuja.ugc123@gmail.com>
2026-05-29 00:23:52 +0530

    Initial project commit

---
+++ attendance/src/main/java/com/anuja/attendance/AttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.anuja.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceApplication.class, args);
+	}
+
+}
