8ee09cd299af89cb1abd762fe9872acfe100f1d6
Anuja Wanigasekara <anuja.ugc123@gmail.com>
2026-05-29 22:46:00 +0530

    Section management - REST APIs, controller layer, service layer, database table, repository

--- attendance/src/main/java/com/anuja/attendance/entity/User.java
+++ attendance/src/main/java/com/anuja/attendance/entity/User.java
@@ -7,9 +7,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.PrePersist;
+// import jakarta.persistence.PreUpdate;
 
 @Entity
 @Table(name = "users")
